5918e04d02811e01b282968a55db10a7cab8d72b
Emre Kesler <emrekesler@outlook.com>
2022-01-13 21:59:43 +0300

    Nuget Settings

---
+++ nuget-packer.cmd
@@ -0,0 +1,5 @@
+dotnet pack RespCache -c Relase -o PackOutputs
+dotnet pack RespCache.Abstractions -c Relase -o PackOutputs
+dotnet pack RespCache.Provider.Abstractions -c Relase -o PackOutputs
+dotnet pack RespCache.Provider.Memory -c Relase -o PackOutputs
+dotnet pack RespCache.Provider.Redis -c Relase -o PackOutputs
